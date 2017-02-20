809bab0362f0b57f920cf904a8c3a32d884a53d1
cookieshake <cookieshake@hotmail.com>
2017-02-20 09:31:54 +0900

    Preprocessing 패티를 Patty와 Burger의 수정내역에 맞추어 수정

--- patties/Preprocessing.py
+++ patties/Preprocessing.py
@@ -4,16 +4,17 @@ from burgering import Burger, Patty
 from burgering.datatypes import *
 import pandas as pd
 
+
 class Preprocessing(Patty):
-    name ='preprocessing'
+    name ='Preprocessing'
     type = 'm'
     title = {'default': 'Preprocessing'}
     description = {'en': 'Check shape and type of the data, select columns and rows and remove outliers',
                    'ko': '데이터 형태를 확인하고 일부 데이터를 선택하거나 이상치를 제거할 수 있습니다'}
 
     author = 'ginger_ale'
-    input_type = 'Data' #TODO change
-    output_type = 'Data' #Todo change
+    input_type = (Data,)
+    output_type = (Data,)
 
     dependencies = []
     properties_format = {}
@@ -48,11 +49,11 @@ class Preprocessing(Patty):
 
 
     def manage(self,data,list):
-        rslt = Burger.communicate(self.burger,self,"이상값이 총 "+len(list)+"개 입니다.",["전부 삭제","개별 확인","변경하지 않음"])
+        rslt = Burger.alert(self.burger, self, "이상값이 총 " + len(list) + "개 입니다.", ["전부 삭제", "개별 확인", "변경하지 않음"])
         if rslt == "전부 삭제":
             result_list = ['행 삭제', '열 삭제', '변경하지 않음']
             mssg = "전체 outlier에 대해 할 행동을 골라주세요"
-            result = Burger.communicate(self.burger, self, mssg, result_list)
+            result = Burger.alert(self.burger, self, mssg, result_list)
             if result == '행 삭제':
                 col =[]
                 for each in list:
@@ -60,7 +61,7 @@ class Preprocessing(Patty):
                         col.append(each[0])
                 self.data = self.remove(self,data,col,'c')
                 mssg = str(col) + " : 삭제되었습니다."
-                Burger.communicate(self.burger,self,mssg,[])
+                Burger.alert(self.burger, self, mssg, [])
             elif result =="열 삭제":
                 row = []
                 for each in list:
@@ -68,7 +69,7 @@ class Preprocessing(Patty):
                         row.append(each[1])
                 self.data = self.remove(self,data,row,'r')
                 mssg = str(row) + " : 삭제되었습니다."
-                Burger.communicate(self.burger,self,mssg,[])
+                Burger.alert(self.burger, self, mssg, [])
 
             else:
                 raise WrongResult(self,result)
@@ -78,15 +79,15 @@ class Preprocessing(Patty):
             for each in list:
                 result_list = ['행 삭제','열 삭제','값 변경','변경하지 않음']
                 mssg = "이상값(%d/%d) : 행 - %s     열 - %d", count,len(list),each[0], each[1]
-                result = Burger.communicate(self.burger,self,mssg,result_list)
+                result = Burger.alert(self.burger, self, mssg, result_list)
                 if result == '행 삭제':
                     self.data = self.remove(self,self.data,each[0],'c')
                 elif result == '열 삭제':
                     self.data = self.remove(self, self.data, each[1], 'r')
                 elif result =='값 변경':
-                    rslt = Burger.communicate(self.burger,self,"수정할 값을 입력하세요")
+                    rslt = Burger.alert(self.burger, self, "수정할 값을 입력하세요")
 
-                    Burger.communicate(self.burger,self,"변경할 값을 입력해주세요 : ", )
+                    Burger.alert(self.burger, self, "변경할 값을 입력해주세요 : ", )
 
                 else:
                     raise WrongResult(self,result)
@@ -105,13 +106,13 @@ class Preprocessing(Patty):
         elif (flag =='r'):
             return data.drop(id,axis=0)
         else:
-            raise paramMismatch
+            raise paramMismatch()
 
     def select_col(self,data,id_list,axis):
         pass
 
-    def burn(self,input_data):
-        self.data = input_data
+    def burn(self, data):
+        self.data = data
 
         #이걸 sequencial하게 하는게 아니라 유저가 선택해야 하는거 아닌가...?
         self.check_null(self.data)
